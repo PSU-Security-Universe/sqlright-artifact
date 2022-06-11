a481dc77ba6f6eaed8df3c91fcf34263e65b3287
Yu Liang <luy70@psu.edu>
2022-06-11 14:30:44 -0400

    Change the setup_mysql.sh to the release version. Avoid saving the intermediate docker build cache.

--- MySQL/scripts/setup_mysql.sh
+++ MySQL/scripts/setup_mysql.sh
@@ -2,10 +2,10 @@
 cd "$(dirname "$0")"/../docker
 
 ## For debug purpose, keep all intermediate steps to fast reproduce the run results.
-sudo docker build --rm=false -f ./Dockerfile -t sqlright_mysql .  
+#sudo docker build --rm=false -f ./Dockerfile -t sqlright_mysql .  
 
 ## Release code. Remove all intermediate steps to save hard drive space.
-#sudo docker build --rm=true -f ./Dockerfile -t sqlright_mysql .
+sudo docker build --rm=true -f ./Dockerfile -t sqlright_mysql .
 
 
 
