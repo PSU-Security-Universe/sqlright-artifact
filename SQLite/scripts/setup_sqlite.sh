431c35041974e28cb72d7f02112f671ad3879045
Yu Liang <luy70@psu.edu>
2022-06-16 20:04:57 -0400

    Added the sqlite bisecting cache to the server.

--- SQLite/scripts/setup_sqlite.sh
+++ SQLite/scripts/setup_sqlite.sh
@@ -1,6 +1,21 @@
 #!/bin/bash -e
 cd "$(dirname "$0")"/../docker
 
+# Get the sqlite_bisecting_binary data ready. 
+if [ -d ./sqlite_bisecting_binary ]; then
+    rm -rf sqlite_bisecting_binary
+fi
+
+if [ ! -d ./sqlite_bisecting_binary ]; then
+    cd sqlite_bisecting_binary_zip
+    cat sqlite_bisecting_binary.zip* > ./sqlite_bisecting_binary_zip.zip
+    echo "Unzipping files. Wait for a second. "
+    unzip sqlite_bisecting_binary_zip.zip &> /dev/null
+    mv sqlite_bisecting_binary ../sqlite_bisecting_binary
+    cd ../
+fi
+
+
 ## For debug purpose, keep all intermediate steps to fast reproduce the run results.
 sudo docker build --rm=false -f ./Dockerfile -t sqlright_sqlite .
 
