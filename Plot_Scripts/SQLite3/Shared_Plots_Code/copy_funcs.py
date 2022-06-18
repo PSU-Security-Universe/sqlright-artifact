bb3a0f2f25fdbe8637e1f5c794c197fb847b9786
Yu Liang <luy70@psu.edu>
2022-06-18 13:13:35 -0400

    If time.txt not exists, do not exit. Just return. copy_func.py, SQLite and MySQL.

--- Plot_Scripts/SQLite3/Shared_Plots_Code/copy_funcs.py
+++ Plot_Scripts/SQLite3/Shared_Plots_Code/copy_funcs.py
@@ -29,7 +29,8 @@ def copy_sqlright_contents_in_dir(src:str, dest:str):
     bug_time_src = os.path.join(bug_time_src, "bug_samples/unique_bug_output")
     if not os.path.isdir(bug_time_src):
         print("Error: time.txt file from: %s not exists. Please run the bisecting algorithm first before calling the copy_results.py. ")
-        exit(1)
+        return
+        #exit(1)
     bug_time_src = os.path.join(bug_time_src, "time.txt")
     if not os.path.isfile(bug_time_src):
         print("Warning: time.txt file from: %s not exists. Did you finish the bisecting algorithm? ")
