f94131fe1966e8feaf506e373afcc41b542fb176
Yu Liang <luy70@psu.edu>
2022-06-18 15:30:11 -0400

    Change the Docker build command to --rm=true.

--- SQLite/scripts/setup_sqlite.sh
+++ SQLite/scripts/setup_sqlite.sh
@@ -17,7 +17,7 @@ fi
 
 
 ## For debug purpose, keep all intermediate steps to fast reproduce the run results.
-sudo docker build --rm=false -f ./Dockerfile -t sqlright_sqlite .
+#sudo docker build --rm=false -f ./Dockerfile -t sqlright_sqlite .
 
 ## Release code. Remove all intermediate steps to save hard drive space.
-# sudo docker build --rm=true -f ./Dockerfile -t sqlright_sqlite .
+sudo docker build --rm=true -f ./Dockerfile -t sqlright_sqlite .
