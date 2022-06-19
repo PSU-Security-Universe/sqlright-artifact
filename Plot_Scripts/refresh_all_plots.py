bf0bf913ce0e523210313d9afdba014ab3406991
Yu Liang <luy70@psu.edu>
2022-06-19 12:28:55 -0400

    Change SQLite Dockerfile. Make sure it is all lower-case to avoid potential Docker build errors.

--- Plot_Scripts/refresh_all_plots.py
+++ Plot_Scripts/refresh_all_plots.py
@@ -4,17 +4,16 @@ import subprocess
 ori_dir = os.getcwd()
 
 for subdir, dirs, files in os.walk("./"):
-    for file in files:
-        
-        # cur_file = os.path.join(subdir, file)
-        cur_file = file
+    for cur_file in files:
         if cur_file[-2:] == "py" and "refresh_all_plots" not in cur_file:
-            print("Change dir: %s, running: %s" % (subdir, file))
-            p = subprocess.Popen(
-                ["python3", cur_file],
-                cwd = subdir
-            )
-            p.wait()
-            print("Finished")
+            if "run_plot" in cur_file: # Only calls run_plots.py
+                print("Change dir: %s, running: %s" % (subdir, cur_file))
+                p = subprocess.Popen(
+                    ["python3", cur_file],
+                    cwd = subdir
+                )
+                p.wait()
+
+print("Finished all plots.")
         
 
