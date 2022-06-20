54127fe2934a66ff9f6de3e719670596b42b0a28
Yu Liang <luy70@psu.edu>
2022-06-19 22:55:02 -0400

    Updated refresh_all_plots.py

--- Plot_Scripts/refresh_all_plots.py
+++ Plot_Scripts/refresh_all_plots.py
@@ -6,8 +6,15 @@ ori_dir = os.getcwd()
 for subdir, dirs, files in os.walk("./"):
     for cur_file in files:
         if cur_file[-2:] == "py" and "refresh_all_plots" not in cur_file:
-            if "run_plot" in cur_file: # Only calls run_plots.py
-                print("Change dir: %s, running: %s" % (subdir, cur_file))
+            if "copy_results.py" in cur_file: # Only calls run_plots.py
+                print("Change dir: %s, running: copy_results.py and run_plots.py" % (subdir))
+                p = subprocess.Popen(
+                    ["python3", cur_file],
+                    cwd = subdir
+                )
+                p.wait()
+
+                run_plot = "run_plots.py"
                 p = subprocess.Popen(
                     ["python3", cur_file],
                     cwd = subdir
